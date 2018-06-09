17fa0d46011c7c4de709edb311f7ccbb3ddd014f
Pia Soy <piasoy4@gmail.com>
2018-06-09 17:53:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ function dec(n){
 }
 
 function makeInt(n, base){
-  var parsed parseInt(n, base)
-  return(n)
+  var parsed = parseInt(n, base)
+  if (isNaN(parsed)) { return 0 }
+  return parsed * 100;
+
 }
 
 function preserveDecimal(n){
